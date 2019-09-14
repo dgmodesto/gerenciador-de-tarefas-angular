4a7c5a0f4e3c0eaa8cd14544ad799f4e7053f24e
dgmodesto <douglasgomesmodesto@gmail.com>
2019-09-14 12:31:53 -0300

    feat(rota-tarefa) configurar rota para acessas funcionlidades de tarefa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { TarefasRoutes } from "./tarefas";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "/tarefas/listar",
+    pathMatch: "full"
+  },
+  ...TarefasRoutes
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
